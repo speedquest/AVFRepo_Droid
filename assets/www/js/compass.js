f299188b538534e32e79944736c4d64d288020cd
Nick Weil <speedquest@fullsail.edu>
2012-05-20 00:42:29 -0400

    Added Media native feature found in book: 20 Recipes for Programming PhoneGap: C

--- assets/www/js/compass.js
+++ assets/www/js/compass.js
@@ -1,4 +1,4 @@
-// Wait for Cordova to load
+/* // Wait for Cordova to load
 //
 document.addEventListener("deviceready", onDeviceReady, false);
 
@@ -18,4 +18,34 @@ function onSuccess(heading) {
 //
 function onError(compassError) {
     alert('Compass Error: ' + compassError.code);
+}
+*/
+
+var watchID = null;
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+    startWatch();
+}
+// Start watching the compass
+function startWatch() {
+    // Update compass every 2 seconds
+    var options = { frequency: 2000 };
+    watchID = navigator.compass.watchHeading(onSuccess, onError, options);
+}
+// Stop watching the compass
+function stopWatch() {
+    if (watchID) {
+        navigator.compass.clearWatch(watchID);
+        watchID = null;
+    }
+}
+// onSuccess: Get the current heading
+function onSuccess(heading) {
+    var element = document.getElementById('heading');
+    element.innerHTML = 'Heading: ' + heading;
+}
+// onError: Failed to get the heading
+function onError() {
+    alert('oooops!');
 }
